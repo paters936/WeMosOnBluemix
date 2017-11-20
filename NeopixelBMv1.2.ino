03fee2fe323d8f5965fe5430ed18c53c0ea15128
Jon paterson <jon@youonly.co.uk>
2017-11-20 21:39:54 +0000

    Update NeopixelBMv1.2.ino
    Cleaned up 
    Removed password to AP
    added extra sections for time out 
    added reset wifi

--- NeopixelBMv1.2.ino
+++ NeopixelBMv1.2.ino
@@ -1,24 +1,20 @@
 /* 
- *  Neopixels on WeMos using IBM Bluemix
-
-Neopixel test using a WeMoS D1 ESP-8266 board and Node-RED on IBM Bluemix
-expects messages of the form: "#rrggbb" and sets the LEDs to that colour
-
-
-By Andy Stanford-Clark - with embellishments by Lucy Rogers and a simple addition of Wifi connection Management by Tim Minter 
-*/
+Neopixels on WeMos using IBM Bluemix By Andy Stanford-Clark - with embellishments by Lucy Rogers and a simple addition of Wifi connection Management by Tim Minter 
+Edited for the Eaglelabs sessions by Jon Paterson */
 
 // You will need to install the "WiFiManager" library by Tzapu via Library Manager.
-
 // If the board is started, and a known wifi connection is available, it will connect automatically. 
 // If no known connection is found the board will switch to access point mode and a wifi network will be created with the ACCESS_POINT_NAME set below. 
 // Using anotherdevice you cna the connect to that wifi connection using the ACCESS_POINT_PASSWORD set below.
 // Your browser will be opened to a configuration page where you can set up the wifi credentials for this board.
-
+// set time out to 60 sec if wrong password or wrong account connected
 
 // ACCESS_POINT_SETUP
 #define ACCESS_POINT_NAME "EnchantedConnection"
 #define ACCESS_POINT_PASSWORD "passwordHere"
+
+//reset settings - for testing
+//wifiManager.resetSettings();
 
  /* 
  * Copyright 2016 IBM Corp.
@@ -34,22 +30,14 @@ By Andy Stanford-Clark - with embellishments by Lucy Rogers and a simple additio
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- 
-
- use board "WeMos D1 R2 & mini"
- CPU 160MHz
- 4M (3M SPIFFS)
- upload speed 115200
- 
 */
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <Adafruit_NeoPixel.h>
-
-#include <DNSServer.h> //Local DNS Server used for redirecting all requests to the configuration portal
-#include <ESP8266WebServer.h> //Local WebServer used to serve the configuration portal
-#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
 
 // remember to change MQTT_KEEPALIVE to 60 in the header file {arduino installation}/libraries/PubSubClient/src/PubsSubClient.h
 
@@ -59,41 +47,30 @@ By Andy Stanford-Clark - with embellishments by Lucy Rogers and a simple additio
 #define BROKER "v4sxgx.messaging.internetofthings.ibmcloud.com"
 // update this with the Client ID in the format d:{org_id}:{device_type}:{device_id}
 // eg d:et6ddf:Wemos:WemosDoorKnob
-#define CLIENTID ""
+#define CLIENTID "d:et6ddf:Wemos:WemosDoorKnob"
 // update this with the authentcation token
-#define PASSWORD ""
+#define PASSWORD "111111111111111"
 
 /////////////////////////////////////////////////////////////////////////////////////////////
-
 
 // subscribe to this for commands:
 #define COMMAND_TOPIC "iot-2/cmd/command/fmt/text"
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+Adafruit_NeoPixel pixel = Adafruit_NeoPixel(2, 4);
 
-//Change this if using different number of neopixels or different pin
-Adafruit_NeoPixel pixel = Adafruit_NeoPixel(1, 4); // eight pixels, on pin 4
-// pin 4 is "D2" on the WeMoS D1 mini
-
-
-// flashes this colour when connecting to wifi:
 static uint32_t wifi_colour = pixel.Color(128, 0, 128); // magenta
 // flashes this colour when connecting to MQTT:
 static uint32_t mqtt_colour = pixel.Color(0, 128, 128); // cyan
-
 static uint32_t current_colour = 0x000000; // black
 static uint32_t current_LED = current_colour;
-
-
 
 void setup() {
   
   Serial.begin(9600);
-
   pixel.begin();
   pixel.show(); // Initialize all pixels to 'off'
-  
   setup_wifi();
   client.setServer(BROKER, 1883);
   client.setCallback(callback);
@@ -102,8 +79,6 @@ void setup() {
 
 
 void setup_wifi() {
-  
-  // connecting to wifi colour
   set_pixels(wifi_colour); 
   
   // Start by connecting to the WiFi network   
@@ -111,10 +86,10 @@ void setup_wifi() {
   Serial.print("Connecting to wifi or setting up access point");
   
   WiFiManager wifiManager;
-  wifiManager.autoConnect("EnchantedSetup","S3tMeUpNow");
+  wifiManager.autoConnect("Coathook#");
   
   wait_for_wifi();
-  
+  wifiManager.setTimeout(60);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   
@@ -123,8 +98,6 @@ void setup_wifi() {
 
 void callback(char* topic, byte* payload, unsigned int length) {
   char content[10];
-  
-  // #rrggbb
   
   Serial.print("Message arrived: ");
 
@@ -201,9 +174,8 @@ void reconnect() {
     }
   }
   
-  set_pixels(0); // clear pixel when connected (black)
+  set_pixels(0);
  
-  // subscribe to the command topic
   client.subscribe(COMMAND_TOPIC);
 }
 
@@ -211,8 +183,6 @@ void reconnect() {
 void set_colour(uint32_t colour) {
   
   set_pixels(colour);
-  // Update current_LED with what the user last requested,
-  // so we can toggle it to black and back again.
   current_colour = colour;
 }
 
@@ -223,9 +193,6 @@ void set_pixels(uint32_t colour) {
     pixel.setPixelColor(i, colour);
   }
   pixel.show();
-
-  // Store current actual LED colour
-  // (which may be black if toggling code turned it off.)
   current_LED = colour;
 }
 
@@ -234,12 +201,10 @@ void toggle_pixel() {
 
   if (current_LED == 0) 
   {
-    // if it's black, set it to the stored colour
     set_pixels(current_colour);
   } 
   else
   {
-    // otherwise set it to black
     set_pixels(0);
   }
 }
